eb08b488f2b75fe8c2f6b74c3de76e6c4476f733
balamutik <ronin1984@gmail.com>
2023-01-16 21:47:33 +0300

    get host by id

--- routes/hosts.js
+++ routes/hosts.js
@@ -34,7 +34,16 @@ module.exports = function (fastify, opts, done) {
         const hosts_coll = fastify.mongo.db.collection('hosts')
         try{
             const hosts = await hosts_coll.find({client_id: req.client_id}).toArray()
-            console.log(hosts)
+            return reply.send(hosts)
+        }catch (e) {
+            return reply.status(500).send('Bad response')
+        }
+    })
+
+    fastify.get('/gethostbyid', async (req, reply)=>{
+        const hosts_coll = fastify.mongo.db.collection('hosts')
+        try{
+            const hosts = await hosts_coll.findOne({client_id: req.client_id, _id: new fastify.mongo.ObjectId(req.query._id)})
             return reply.send(hosts)
         }catch (e) {
             return reply.status(500).send('Bad response')
