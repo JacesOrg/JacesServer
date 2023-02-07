98672b0c354983c4e1d4f91a2572e5d61c141500
balamutik <ronin1984@gmail.com>
2023-02-07 23:43:34 +0300

    added container stat get/set

--- routes/containers.js
+++ routes/containers.js
@@ -6,7 +6,7 @@ module.exports = function (fastify, opts, done) {
             const containerColl = fastify.mongo.db.collection('containers')
             const stat = req.body
 
-            const cont = containerColl.findOne({client_id: req.client_id, container_id: container_id})
+            const cont = await containerColl.findOne({client_id: req.client_id, container_id: container_id})
             if(!cont)
                 return reply.status(404).send({success: false, message: 'Container not found'})
             await containerColl.updateOne({client_id: req.client_id, container_id: container_id}, {$set: stat})
@@ -21,10 +21,62 @@ module.exports = function (fastify, opts, done) {
         try{
             const container_id = req.params.container_id;
             const containerColl = fastify.mongo.db.collection('containers')
-            const cont = containerColl.findOne({client_id: req.client_id, container_id: container_id})
+            const cont = await containerColl.findOne({client_id: req.client_id, container_id: container_id})
             if(!cont)
                 return reply.status(404).send({success: false, message: 'Container not found'})
             return reply.send(cont)
+        }catch (e) {
+            console.log(e)
+            return reply.send({success: false, message:  e.message})
+        }
+    })
+
+    fastify.get('/actions/get/:host_id', async (req, reply)=>{
+        try{
+            const host_id = req.params.host_id;
+            const actionColl = fastify.mongo.db.collection('actions')
+            const actions = await actionColl.find({client_id: req.client_id, host_id: host_id}).sort({created: 1}).toArray()
+            return reply.send(actions)
+        }catch (e) {
+            console.log(e)
+            return reply.send({success: false, message:  e.message})
+        }
+    })
+
+    fastify.post('/actions/set/:host_id', async (req, reply)=>{
+        try{
+            const host_id = req.params.host_id;
+            const action = req.body
+            action.client_id = req.client_id
+            action.host_id = host_id
+            const actionColl = fastify.mongo.db.collection('actions')
+            const actions = await actionColl.insertOne(action)
+            return reply.send({success: true, id: actions.insertedId})
+        }catch (e) {
+            console.log(e)
+            return reply.send({success: false, message:  e.message})
+        }
+    })
+
+    fastify.get('/actions/status/:action_id', async (req, reply)=>{
+        try{
+            const action_id = req.params.action_id;
+            const actionColl = fastify.mongo.db.collection('actions')
+            const action = await actionColl.findOne({_id: new fastify.mongo.ObjectId(action_id)})
+            return reply.send({success: true, status: action.status, message: action.message})
+        }catch (e) {
+            console.log(e)
+            return reply.send({success: false, message:  e.message})
+        }
+    })
+
+    fastify.get('/actions/update/:action_id', async (req, reply)=>{
+        try{
+            const action_id = req.params.action_id;
+            const {status, message} = req.body
+            const actionColl = fastify.mongo.db.collection('actions')
+            await actionColl.updateOne({_id: new fastify.mongo.ObjectId(action_id)}, {$set: {status: status, message: message}})
+            return reply.send({success: true})
         }catch (e) {
             console.log(e)
             return reply.send({success: false, message:  e.message})
